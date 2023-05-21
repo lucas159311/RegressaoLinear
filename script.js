7c0974532d2e3fa073ecf7ae0c083604d2b14dd3
Lucas Avelino <50250488+lucas159311@users.noreply.github.com>
2023-05-21 12:56:37 -0300

    Finalizando implementação
    Realizando a atribuição do tipo dos números como Integer e adicionando a verificação dos campos.

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ function pegaDados(){
     var inputX = document.getElementById('X').value;
     var inputY = document.getElementById('Y').value;
 
-    //console.log(inputX);
-    //console.log(inputY);    
+    //verificando se os campos estão preenchidos
+    if((inputX == "") || (inputY == "")){
+      alert("Preencha todos os campos para continuar...");
+    }else{
+      //console.log(inputX);
+      //console.log(inputY);    
+      var auxX = inputX.split(',');
+      var auxY = inputY.split(',');
 
-    var returnX = inputX.split(',');
-    var returnY = inputY.split(',');
 
-    return{
-        returnX,
-        returnY
-    };
+      //verificando se a quantidade de numeros digitados são iguais
+      if(auxX.length != auxY.length){
+        alert("A quantidade de numeros em X deve ser igual em Y");
+      }else{
+        var returnX = new Array();
+        var returnY = new Array();
+    
+        //transformando os valores resgatados em numeros
+        for(var i=0; i < auxX.length; i++){
+          returnX.push(parseInt(auxX[i]));
+          returnY.push(parseInt(auxY[i]));
+        }
+    
+        return{
+            returnX,
+            returnY
+        };
+      }
+    }
+
+    
 }
 
 function Limpar(){
@@ -24,10 +45,15 @@ function Limpar(){
 
 function Calcular(){
 
-    pegaDados();
-    /*
-    var x = new Array(1,2,-1,0)//dados.returnX;
-    var y = new Array(1,3,2,-1)//dados.returnY;
+    var dados = pegaDados();
+
+    console.log(dados.returnX);
+    console.log(dados.returnY);
+
+    var x = dados.returnX;//new Array(1,2,-1,0)//dados.returnX;
+    var y = dados.returnY;//new Array(1,3,2,-1)//dados.returnY;
+  
+    //0,5 e 1
 
     const n = Math.min(x.length, y.length);
     // Calcular as somas dos valores
@@ -45,12 +71,23 @@ function Calcular(){
   
     // Calcular os coeficientes da regressão
     const angular = (n * SomaXY - SomaX * SomaY) / (n * SomaXX - SomaX * SomaX);
-    const linear = (SomaY - angular * SomaX) / n;
+    const linear = ((SomaY - angular * SomaX) / n);
     console.log(angular);
     console.log(linear);
-    return {
-      angular: angular,
-      linear: linear
-    };
-    */
+
+    // Y = linear +/- angular * X
+    ExibeResultado(angular, linear);
+}
+
+function ExibeResultado(angular, linear){
+  var exibicao = document.getElementById('resposta');
+
+  exibicao.innerHTML = "";
+  if(angular >= 0){ //verifico se ele é positivo
+    exibicao.innerHTML = '<p style="margin-top: 2%; font-weight: bold" align="center">Y=' + linear + '+' + angular + 'X</p>';
+  }else{
+    exibicao.innerHTML = '<p style="margin-top: 2%; font-weight: bold" align="center">Y=' + linear + '-' + angular + 'X</p>';
+  }
+  
+
 }
